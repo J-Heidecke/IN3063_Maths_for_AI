aed96559972cddccbba4ef5f966488be39509d1d
Johannes Heidecke <johannes.heidecke@city.ac.uk>
2020-11-15 21:17:35 +0000

    New_Dijkstra

--- task_1/Dijkstra.py
+++ task_1/Dijkstra.py
@@ -10,6 +10,7 @@ from random import randint
 import numpy as np
 import sys 
 
+'''
 g = []
 width = 3
 height = 3
@@ -23,8 +24,24 @@ for row in range(height):
 # Convert to numpy.array
 grid = np.array(g)
 print(grid)
+'''
+width = 10
+height = 10
+seed(6)
 
-class Dijkstra:
+def make_grid(h, w):
+    g = []
+    for r in range(h):
+        g.append([])
+        for c in range(w):
+            g[r].append(randint(0,9))
+            
+    output = np.array(g)   
+    return output
+
+grid = make_grid(height, width)
+
+class Convert:
     
     def __init__(self, grid, source, finish):
         self.grid = grid
@@ -115,7 +132,130 @@ def shortestpath(graph,start,end,visited=[],distances={},predecessors={}):
     # now take the closest node and recurse, making it current 
     return shortestpath(graph,closestnode,end,visited,distances,predecessors)
 
-d = Dijkstra(grid, src, finish)
+d = Convert(grid, src, finish)
 graph = d.convert_grid()
 
 print (shortestpath(graph,str(s),str(e)))
+
+
+# -- Dijkstra new ---
+
+# https://www.youtube.com/watch?v=IG1QioWSXRI
+def dijkstra(graph, start, goal):
+    shortest_distance = {}
+    predecessor = {}
+    unseenNodes = list(graph.keys())
+    inf = 999999
+    path = []
+    
+    for node in unseenNodes:
+        shortest_distance[node] = inf
+    shortest_distance[start] = 0
+    
+    while unseenNodes:
+        minNode = None
+        for node in unseenNodes:
+            if minNode is None:
+                minNode = node
+            elif shortest_distance[node] < shortest_distance[minNode]:
+                minNode = node
+
+        for childNode, weight in graph[minNode].items():
+            if weight + shortest_distance[minNode] < shortest_distance[childNode]:
+                shortest_distance[childNode] = weight + shortest_distance[minNode]
+                predecessor[childNode] = minNode
+        unseenNodes.remove(minNode)
+    
+    current = goal
+    while current != start:
+        try:
+            path.insert(0, current)
+            current = predecessor[current]
+        except KeyError:
+            print('Path not readable')
+            break
+    path.insert(0, start)
+
+    if shortest_distance[goal] != inf:
+        print(shortest_distance[goal])
+
+
+
+test = make_grid(4,4)
+src = [0,0]
+finish = [4,4]
+d = Convert(test, src, finish)
+graph = d.convert_grid()
+dijkstra(graph, '0', '202')
+'''
+paths = []
+for i in range(3,101):
+    src_ = [0,0]
+    finish_ = [i, i]
+    grid_ = make_grid(i, i)
+    d_ = Convert(grid_, src_, finish_)
+    graph_ = d.convert_grid()
+    s_ = (src_[0] * 100) + src_[1]
+    e_ = ((finish_[0] * 100) + finish_[1]) - 101
+    
+    paths.append(dijkstra(graph_, str(s_), str(e_)))
+print(paths)
+'''
+# -- Analytics ---
+'''
+class Analytics:
+    
+    def __init__(self, algorithm):
+        self.algorithm = algorithm
+    
+    def make_grid(self,w, h):
+        g = []
+        seed(70)
+        for r in range(w):
+            g.append([])
+            for c in range(h):
+                g[r].append(randint(0,9))
+            
+        output = np.array(g)   
+        return output
+    
+    def ant(self):
+        pass;
+
+    def dijkstra(self):
+        pass;
+    
+    def baseline(self):
+        paths = []
+    
+        for i in range(3, 101):
+            grid = self.make_grid(i,i)
+            b = Baseline(grid)
+            path = b.algorithm()
+            paths.append(path)
+
+        return paths
+            
+    def run(self):
+        if(self.algorithm == 'base'):
+            baseline_results = self.baseline()
+            return baseline_results
+        elif(self.algorithm == 'dijk'):
+            dijkstra_results =  self.dijkstra()
+            return dijkstra_results
+        elif(self.algorithm == 'ant'):
+            print('Not implemented yet')
+        else:
+            print('Algorithm not defined')
+    
+    def plot(self):
+        results = self.run()
+        plt.plot(results)
+        plt.xlabel('Run')
+        plt.ylabel('Length of Path Selected')
+        
+        return ('Mean: %.2f' %np.mean(results))
+    
+analytics = Analytics('base')
+analytics.plot()
+'''
