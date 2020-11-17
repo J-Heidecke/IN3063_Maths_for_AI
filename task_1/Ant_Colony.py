ff8f8b318d5eae38d5e98c1bf17d4206ea187457
unknown <imaum2208@gmail.com>
2020-11-17 23:34:21 +0000

    Mostly done, need& C:/Users/Aum/AppData/Local/Microsoft/WindowsApps/python.exe c:/Users/Aum/Documents/maths_ai/IN3063_Maths_for_AI/task_1/Ant_Colony.py

--- task_1/Ant_Colony.py
+++ task_1/Ant_Colony.py
@@ -1,10 +1,11 @@
 import numpy as np
 
+
 class AnyColony(object):
     def __init__(self, height, width, decayRate, numberOfAnts, pheromoneWeighting, timeWeighting):
         self.decayRate = decayRate  # rate at which the pheremones vaporise
         self.numberOfAnts = numberOfAnts  # the number of ants to be used
-
+        
         # Values used to prioritise either pheromone deposits or time taken on squares
         self.pheromoneWeighting = pheromoneWeighting
         self.timeWeighting = timeWeighting
@@ -15,34 +16,51 @@ class AnyColony(object):
         # setup a grid with random numbers 0,9
         self.grid = np.random.randint(0, 10, size=(height, width))
         # will store the pheromone value in correspondence to position in the grid # initialise a grid with 0 of size height x width
-        self.pheromoneDeposits = np.full((height, width), 0)
+        self.pheromoneDeposits = np.full((height, width), 1)
 
         # Start and end points for the ants to reach
         self.startIndex = [0, 0]
         self.endIndex = [height-1, width-1]
-
+        
+        self.shortestPath = []
         #### TESTING ####
         self.generateFixedGrid()
 
     # TEST CASE
     def generateFixedGrid(self):
         self.grid = np.array([
-            [0, 4, 7, 2],
+            [1, 4, 7, 2],
             [3, 6, 5, 3],
             [1, 2, 2, 1],
-            [8, 6, 9, 0]
+            [8, 6, 9, 1]
         ])
         # will store the pheromone value in correspondence to position in the grid # initialise a grid with 0 of size height x width
-        self.pheromoneDeposits = np.full((4, 4), 0)
+        self.pheromoneDeposits = np.full((4, 4), 1)
+        self.width = 4
+        self.height = 4
+        self.endIndex = [3,3]
 
     # calculate the score of a Square
     def calculateSquareScore(self, x, y):
         # multiply the pheromone with 1/timeTakenOnTheGrid (multiply the scalars as well)
-        return (self.pheromoneDeposits[x, y] * self.pheromoneWeighting) * ((1 / self.grid[x, y])*self.timeWeighting)
+        print("\nCALCULATE SCORE:")
+        print("Grid value: ", self.grid[x,y])
+        print("Pheremone Deposit: ", self.pheromoneDeposits[x,y])
+        score = (self.pheromoneDeposits[x, y] ** self.pheromoneWeighting) * ((1 / self.grid[x, y])**self.timeWeighting)
+        print(score)
+        return score
 
     # deposit the appropriate amount of pheremones at the certain position
-    def depositPheromones(self, x, y):
-        self.pheromoneDeposits[x, y] = 1 / self.grid[x, y]
+    def depositPheromones(self, visitedSquares):
+        # self.pheromoneDeposits[x, y] = 1 / self.grid[x, y]
+        pathLength = 0
+        for square in visitedSquares:
+            pathLength += self.grid[square[0], square[1]]
+        print("Path Length: ", pathLength)
+        pheremoneAmount = 15/(pathLength/len(visitedSquares))
+        for square in visitedSquares:
+            self.pheromoneDeposits[square[0], square[1]] += pheremoneAmount
+            print("Pheremone Square = ",  self.pheromoneDeposits[square[0], square[1]])
 
     # decay the pheromones that were placed in previous generations
     def decayPheromones(self):
@@ -56,6 +74,8 @@ class AnyColony(object):
         # iteratively go through every generation
         for i in range(self.numberOfAnts):
             currentPosition = self.startIndex
+            visitedSquares = [] # the squares this and has visited
+
 
             # find available squares
             # find probability on each of them
@@ -63,10 +83,100 @@ class AnyColony(object):
             # record the square it has gone too (needed to add pheromone values)
             # REPEAT until it reaches the end index
             #
-            print(i)
+
+           
+            hasReachedTheEnd = False            
+            # loop until the ant has reached the end 
+            while not hasReachedTheEnd:
+                print(self.grid)
+
+                print("\nCurrent Square: ", currentPosition)
+                # store coordinates of the available squares
+                availableSquares = [[currentPosition[0]+1, currentPosition[1]], [currentPosition[0]-1, currentPosition[1]], [
+                    currentPosition[0], currentPosition[1]+1], [currentPosition[0], currentPosition[1]-1]]
+
+                # Remove squares out of bound
+                tempSquares = []
+                for square in availableSquares:
+                    if (not(square[0] < 0 or square[0] > self.width-1 or square[1] < 0 or square[1] > self.height-1)):
+                        tempSquares.append(square)
+                availableSquares = tempSquares
+                
+                # Remove squares in visited
+                # for i in range(len(visitedSquares)-1):
+                     
+                #     availableSquares.pop(i)
+                for square in visitedSquares:
+                    if (square in availableSquares):
+                        availableSquares.remove(square)
+                    
 
 
-antColony = AnyColony(10, 10, 1, 10, 1, 1)
+                print("Available Squares: ",availableSquares)
+
+                # Calculate the square score
+                squareScore = []
+                for square in availableSquares:
+                    # print("DEBUG\nAvailable Squares:" , availableSquares, "\nSquare:", square, "\n",)
+                    if(square[0] == self.endIndex[0] and square[1] == self.endIndex[1]):
+                        squareScore.append(10000)
+                    else:
+                        squareScore.append(
+                            self.calculateSquareScore(square[0], square[1]))
+                        
+
+                print("Square Score: ", squareScore)
+
+                # Get the probabilities of each square
+                probabilities = []
+                for score in squareScore:
+                    probabilities.append(score/len(squareScore))
+
+                # the list of probabilities for each square
+                print("Probability: ", probabilities)
+
+                toChoose = -1  # which square to chose based on position int the availableSquares array
+                probAdditive = 0
+                randNo = np.random.uniform(0, np.sum(probabilities))
+
+                # Calculate  which square to chose based on the probability.
+                for probability in probabilities:
+                    toChoose += 1  # Which square to choose
+                    if (probability+probAdditive < randNo):
+                        break  # break out of the loop as on has been chosen
+                    probAdditive += probability  # add to the additive so that it can compare properly
+
+                print("Chosen: ", toChoose) # index of the chosen square
+
+                # if it has nowhere to go then move onto the next ant
+                if(toChoose == -1):
+                    print("\n******************\nANT ", i+1," Destroyed \n*****************")
+                    print("Visited Squares")
+                    for square in visitedSquares:
+                        print (square)
+                    break
+ 
+ 
+                visitedSquares.append(currentPosition)
+                currentPosition = availableSquares[toChoose]
+                if (currentPosition[0] == self.endIndex[0] and currentPosition[1] == self.endIndex[1]):
+                    print("\n******************\nANT ", i+1," Made it \n*****************")
+                    print("Visited Squares")
+                    for square in visitedSquares:
+                        print (square)
+                    hasReachedTheEnd = True
+                    self.depositPheromones(visitedSquares)
+                print("Pheremones")
+                print(self.pheromoneDeposits)
+                print()
+            self.decayPheromones()
+                
+
+            
+
+
+
+antColony = AnyColony(10, 10, 0.7, 4, 1, 1)
 antColony.run()
 
 # # Arguments
