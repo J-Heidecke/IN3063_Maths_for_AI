f338ffc37fd72509f9e17fa2c735a69a0c63491c
Aum <imaum2208@gmail.com>
2020-12-10 18:15:50 +0000

    task 3 start

--- task_3/task_3.py
+++ task_3/task_3.py
@@ -3,9 +3,21 @@ import torch
 from torchvision import transforms, datasets
 import torch.nn as nn
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
 from sklearn.datasets import load_digits
 
-# import dataframe
-df = load_digits()
+# import dataframe # reduced version of the MNIST dataset
+digits = load_digits()
+df = torchvision.datasets.MNIST
+# print(df.train_data)
+trainset = datasets.MNIST(root='./data', train=True, download=True, transform=None)
+testset = datasets.MNIST(root='./data', train=False, download=True, transform=None)
 
-print(df)
+
+# print(digits.DESCR)
+# print(digits.keys())
+
+plt.imshow(digits.images[1010], cmap=plt.cm.gray_r, interpolation='nearest')
+plt.show()
+plt.imshow(trainset.data[1011], cmap=plt.cm.gray_r, interpolation='nearest')
+plt.show()
